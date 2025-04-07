0f85f420b45921a298da3961461e41a6a21f37bf
Rik Adriaensen <rik.adriaensen@kuleuven.be>
2025-04-07 11:00:57 +0200

    add wiki_countries bash file

---
+++ run_wikicountries.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python run_experiments.py wiki_countries deepsoftlog/experiments/wiki_countries/config_S0.yaml
+python run_experiments.py wiki_countries deepsoftlog/experiments/wiki_countries/config_S1.yaml
+python run_experiments.py wiki_countries deepsoftlog/experiments/wiki_countries/config_S2.yaml
+python run_experiments.py wiki_countries deepsoftlog/experiments/wiki_countries/config_S3.yaml
